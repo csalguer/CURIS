5512c12b77c77d9678b5aed72b35839aed5ef406
Chris Salguero <cfsalguero93@gmail.com>
2017-09-01 11:02:55 -0700

    Prelude to deployment. Needs look at auth headers and bot selection/redirection.

--- bot_therapy/public/chat.js
+++ bot_therapy/public/chat.js
@@ -21,6 +21,35 @@ var WELCOME_INTENT = 'WELCOME'
 var max_num_sessions = 3;
 var curr_session;
 
+var bot_delivered = null;
+var script_already_init = false;
+
+
+
+
+const Chatbots = {
+    POSITIVE_THINKING : `sunshine`,
+    WORST_CASE : `doom`,
+    PROBLEM_SOLVING : `sherlock`,
+    MASTER : 'bot_therapy'
+}
+
+const Groups = {
+    CONTROLLED : 'control',
+    VARIABLE : 'variable'
+}
+
+
+//To be later stored elsewhere?
+const accessTokens = {};
+//Allows using Chatbots dictionary for reference
+accessTokens[Chatbots.MASTER] = `28f525293d7f40fcbcb30c5651daecce`;
+accessTokens[Chatbots.POSITIVE_THINKING] = `b47b9ed50864483ca0690438a8320074`;
+accessTokens[Chatbots.PROBLEM_SOLVING] = `b70bc3279cb34b349c81b0b11972cd1e`;
+accessTokens[Chatbots.WORST_CASE] = `b6e735085af5459592725fd02a41c121`;
+
+
+
 class Message {
     constructor(text, message_side) {
         if (typeof text === 'undefined' || text === null) {
@@ -56,26 +85,19 @@ class Message {
  * To be executed upon complete loading of resources and DOM element creation
  */
 $(document).ready(function() {
-    // launchWelcomeIntent();
-    // $(document).on("click", "#signin", function(){
-    //   signInHandler();
-    // });
 
     // Handle User Login for 3-Day studies
     $('#login_form').on('submit', function(event) {
         event.preventDefault();
-        switchToLoadingScreen();
         signInHandler();
     });
     $('#signin').click(function(event) {
         event.preventDefault();
-        switchToLoadingScreen();
         signInHandler();
     });
     $('#user_email').keyup(function(event) {
         if (event.which == 13) {
             event.preventDefault();
-            switchToLoadingScreen();
             signInHandler();
         }
     });
@@ -85,6 +107,7 @@ $(document).ready(function() {
         if (event.which == 13) {
             event.preventDefault();
             var user_input = getUserInput();
+            if(user_input.length === 0) {return;}
             setTimeout(addTextToChatHistory(user_input, false), 300);
             setUserInput('');
             sendAndUpdateChat(user_input);
@@ -92,6 +115,7 @@ $(document).ready(function() {
     });
     $('.send_message').click(function(event) {
         var user_input = getUserInput();
+        if(user_input.length === 0) {return;}
         setTimeout(addTextToChatHistory(user_input, false), 300);
         setUserInput('');
         sendAndUpdateChat(user_input);
@@ -103,8 +127,8 @@ const signInHandler = () => {
     console.log('signInHandler');
     let first_name = $('#user_fname').val();
     let last_name = $('#user_lname').val();
-    if (first_name < 1) { alert('First name must be longer than 1 character'); return; }
-    if (last_name < 1) { alert('Last name must be longer than 1 character'); return; }
+    if (first_name < 1) { alert('First name must be longer than 1 character'); return switchToLoginScreen(); }
+    if (last_name < 1) { alert('Last name must be longer than 1 character'); return switchToLoginScreen(); }
     let email = $('#user_email').val();
     let pass = last_name.toLowerCase() + first_name.toLowerCase() + email.split('@')[0];
     let userObj = {
@@ -117,44 +141,73 @@ const signInHandler = () => {
         .then(function(user) {
             console.log('User successfully signed in');
             userObj.uid = user.uid;
-            let returnCheck = checkIfFinishedMoreThanMaxNumSessions(userObj);
+            let returnCheck = checkIfMaxSessionReachedAndGetGroupingExperience(userObj);
             console.log(returnCheck);
         }).catch(function(error) {
             var errorCode = error.code;
             var errorMessage = error.message;
+            console.log(errorMessage);
             if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
-                switchToLoginScreen();
+                swal({
+                    title: "Wrong Password",
+                    text: "Incorrect password was entered.",
+                    type: "error"
+                });
             } else if (errorCode === 'auth/user-not-found') {
+                switchToLoadingScreen();
                 createUserHandler(userObj, pass); //Might have to rework code-example since promise based
                 return;
             } else {
-                alert(errorMessage);
+                swal({
+                    title: "Error",
+                    text: errorMessage,
+                    type: "error"
+                });
             }
-            console.log(error);
         });
 };
 
+
+// NOTE: Main method where userObj gets filled and global vars for chat are assigned
+//       for the NEW_USER case
 const createUserHandler = (userObj, pass) => {
     firebase.auth().createUserWithEmailAndPassword(userObj.email, pass)
         .then(function(user) {
             console.log('User successfully created: ' + user.uid);
+            //Handle all new user specific information assignment and saving here
+            //Choose which group to add to (CTRL vs VRBL), add bot to bot list & save to DB
             userObj.uid = user.uid;
-            return setTimeout(switchToChatUI(true, userObj), 4500);
+            userObj.group = determineSubjectGrouping();
+            //Using an empty history, update bot global var
+            bot_delivered = determineBotToInteractWith([]);
+            userObj.bots_interacted = [bot_delivered];
+            console.log('New user from ' + userObj.group + ' will be interacting with ' + bot_delivered + 'Bot...');
+            return setTimeout(switchToChatUI(true, userObj, bot_delivered), 4500);
         }).catch(function(error) {
             var errorCode = error.code;
             var errorMessage = error.message;
             if (errorCode == 'auth/weak-password') {
-                alert('The password is too weak.');
+                swal({
+                    title: "Weak Password",
+                    text: errorMessage,
+                    type: "error"
+                });
             } else {
-                alert(errorMessage);
+                swal({
+                    title: "Error",
+                    text: errorMessage,
+                    type: "error"
+                });
             }
+            switchToLoginScreen();
             console.log(error);
         });
 };
 
 
-const checkIfFinishedMoreThanMaxNumSessions = userObj => {
+// NOTE: Main method where userObj gets filled and global vars for chat are assigned
+//       for the USER_ALREADY_EXISTS case
+const checkIfMaxSessionReachedAndGetGroupingExperience = userObj => {
     console.log('Checking if n # of sessions already complete...');
     const usersRef = firebase.database().ref().child('users/' + userObj.uid);
     usersRef
@@ -164,10 +217,22 @@ const checkIfFinishedMoreThanMaxNumSessions = userObj => {
                 let data = snap.val();
                 console.log(data.chat_sessions);
                 let convo_sessions = Object.keys(data.chat_sessions)
-                if(convo_sessions.length >= max_num_sessions) {
+                if (convo_sessions.length >= max_num_sessions) {
                     return switchToLockoutScreen();
                 }
-                return setTimeout(switchToChatUI(false, userObj), 4500);
+                //Complete filling in of userObj by updating group attr of the object
+                userObj.group = data.group;
+                if(userObj.group == Groups.CONTROLLED){
+                    //Handle selecting same one assigned
+                    let assignedBot = data.bots_interacted;
+                    bot_delivered = assignedBot[0];
+                }else if(userObj.group == Groups.VARIABLE){
+                    let history = data.bots_interacted;
+                    bot_delivered = determineBotToInteractWith(history);
+                }
+                console.log(data.group);
+                console.log(bot_delivered);
+                return setTimeout(switchToChatUI(false, userObj, bot_delivered), 4500);
             }
             return switchToErrorScreen();
         }).catch(error => {
@@ -176,12 +241,53 @@ const checkIfFinishedMoreThanMaxNumSessions = userObj => {
         });
 };
 
+// NOTE: Not used in chat functionality of chat.js
+//       Convenience method written for DB manipulation and info extraction
+//       Checking of grouping status and appropriate redirection handled by
+//       checkIfMaxSessionReachedAndGetGroupingExperience function listed above
+const retrieveUserSubjectGrouping = userObj => {
+    console.log('Getting user\'s subject group for the study...');
+    const usersRef = firebase.database().ref().child('users/' + userObj.uid);
+    usersRef
+        .once('value')
+        .then(snap => {
+            if (snap.exists()) {
+                let data = snap.val();
+                console.log(data.group);
+                let participant_group = data.group;
+                //Return grouping here or do stuff with grouping and then return
+            }
+        }).catch(error => {
+            console.log(error);
+            return switchToErrorScreen();
+        });
+};
+
+const determineSubjectGrouping = () => {
+    //TODO: Implement groupingGenerator function. 50/50 split as stub method
+    let coin_flip = Math.random() < 0.5;
+    return coin_flip ? Groups.CONTROLLED : Groups.VARIABLE;
+};
+
+
+// NOTE: history must be an array
+const determineBotToInteractWith = history => {
+    let allBots = Object.values(Chatbots).filter( val => {return val != Chatbots.MASTER});
+    let options = allBots.filter( val => {return !history.includes(val)}, history);
+    console.log('Given the history: ' + history + ' , options are: ' + options);
+    return selectRandValue(options);
+};
+
+const selectRandValue = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+};
 
 const addToStudyParticipantsList = (userObj, first_session) => {
     const usersRef = firebase.database().ref().child('users');
     console.log('Attempting to set data to DB using object instead.');
-    // To use a variable as a named property, must be set using Array notation!
-    let data = { chat_sessions: {} };
+    // NOTE: userObj.group MUST be set and determined before adding to the study participants list
+    //       i.e. userObj MUST have been updated before passing onto any switchToChatUI() call
+    let data = { chat_sessions: {}, group: userObj.group, bots_interacted: userObj.bots_interacted };
     data.chat_sessions[first_session] = false;
     console.log('Object\'s data as string value: ', JSON.stringify(data));
     usersRef.child(userObj.uid).set(data);
@@ -196,9 +302,10 @@ const addNewChatSessionToConversationsList = session_id => {
     chatdialoguesRef.update(data);
 };
 
-
+// NOTE: Not used in chat functionality of chat.js
+//       Convenience method written for DB manipulation and info extraction
 const retrieveCurrentChatFromConversationsList = () => {
-    const chatdialoguesRef = fiarebase.database().ref().child('chat_dialogues');
+    const chatdialoguesRef = firebase.database().ref().child('chat_dialogues');
     chatdialoguesRef.child(curr_session)
         .once('value')
         .then(snap => {
@@ -208,6 +315,7 @@ const retrieveCurrentChatFromConversationsList = () => {
                 let dialogue = data.dialogue;
                 console.log('Type of data snapshot: ', (typeof dialogue));
                 console.log(dialogue);
+                //Return or do stuff with dialogue and then return here
             }
         }).catch(error => {
             console.log(error);
@@ -226,6 +334,20 @@ const updateChatInConversationsList = logged_response => {
     chatdialoguesRef.set(chat);
 };
 
+const updateBotInteractionsInUser = (user_id, bot_interacted_with) => {
+    const updateRefPath = 'users/' + user_id;
+    const userRef = firebase.database().ref().child(updateRefPath);
+    let botList;
+    let userObj;
+    userRef.on('value', snap => { snap.exists() ? userObj = snap.val() : {}; });
+    console.log('botList so far...\n', userObj.bots_interacted);
+    console.log('Pushing the current logged response... ', bot_interacted_with);
+    botList = new Set(userObj.bots_interacted);
+    botList.add(bot_interacted_with);
+    userObj.bots_interacted = Array.from(botList);
+    userRef.update(userObj);
+};
+
 
 const addNewChatSessionToUser = (user_id, new_session) => {
     const updateRefPath = 'users/' + user_id + '/chat_sessions';
@@ -236,14 +358,16 @@ const addNewChatSessionToUser = (user_id, new_session) => {
 };
 
 
-const switchToChatUI = (isNewUser, userObj) => {
+const switchToChatUI = (isNewUser, userObj, bot) => {
     curr_session = uuidv4();
     console.log('Session Generated: $' + curr_session);
-    if(isNewUser){
+    if (isNewUser) {
         addToStudyParticipantsList(userObj, curr_session);
+
     }
     addNewChatSessionToUser(userObj.uid, curr_session);
     addNewChatSessionToConversationsList(curr_session);
+    updateBotInteractionsInUser(userObj.uid, bot);
     launchWelcomeIntent(isNewUser, userObj);
     hideAll();
     $('.chat_window').show('fast');
@@ -313,7 +437,7 @@ function launchWelcomeIntent(isNewUser, userObj) {
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         headers: {
-            "Authorization": "Bearer " + accessToken
+            "Authorization": "Bearer " + accessTokens[Chatbots.MASTER]
         },
         data: JSON.stringify(eventObj),
         beforeSend: function() {
@@ -375,7 +499,7 @@ function sendAndUpdateChat(text) {
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         headers: {
-            "Authorization": "Bearer " + accessToken
+            "Authorization": "Bearer " + accessTokens[bot_delivered]
         },
         data: JSON.stringify({ query: text, lang: "en", sessionId: curr_session }),
         beforeSend: function() {
